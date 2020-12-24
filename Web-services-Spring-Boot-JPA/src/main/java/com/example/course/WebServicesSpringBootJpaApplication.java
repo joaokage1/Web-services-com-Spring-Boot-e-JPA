c3974bcb94f010e1010c7138d38b6306780bcd37
João Vitor Gomes Silva <34372105+joaokage1@users.noreply.github.com>
2020-12-24 09:49:40 -0300

    initial commit

---
+++ Web-services-Spring-Boot-JPA/src/main/java/com/example/course/WebServicesSpringBootJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServicesSpringBootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServicesSpringBootJpaApplication.class, args);
+	}
+
+}
---
+++ Web-services-Spring-Boot-JPA/src/test/java/com/example/course/WebServicesSpringBootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServicesSpringBootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
