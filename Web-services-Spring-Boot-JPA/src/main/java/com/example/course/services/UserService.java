914ada4bddb593dd8d89f68b42ea69d1a44bba70
João Vitor Gomes Silva <34372105+joaokage1@users.noreply.github.com>
2020-12-25 10:14:09 -0300

    Exception handling - update

--- Web-services-Spring-Boot-JPA/src/main/java/com/example/course/services/UserService.java
+++ Web-services-Spring-Boot-JPA/src/main/java/com/example/course/services/UserService.java
@@ -3,6 +3,8 @@ package com.example.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -47,9 +49,13 @@ public class UserService {
 	}
 
 	public User updateUser(Long id, User user) {
-		User entity = getRepository().getOne(id);
-		updateData(entity, user);
-		return getRepository().save(entity);
+		try {
+			User entity = getRepository().getOne(id);
+			updateData(entity, user);
+			return getRepository().save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User user) {
