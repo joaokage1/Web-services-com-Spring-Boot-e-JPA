c6787c9d32507e414830a6a2efa02d8830338d20
João Vitor Gomes Silva <34372105+joaokage1@users.noreply.github.com>
2020-12-25 10:11:38 -0300

    Exception handling - delete

--- Web-services-Spring-Boot-JPA/src/main/java/com/example/course/services/UserService.java
+++ Web-services-Spring-Boot-JPA/src/main/java/com/example/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Component;
 
 import com.example.course.model.User;
 import com.example.course.repositories.UserRepository;
+import com.example.course.services.exceptions.DatabaseException;
 import com.example.course.services.exceptions.ResourceNotFoundException;
 
 @Component
@@ -34,7 +37,13 @@ public class UserService {
 	}
 
 	public void deleteUser(Long id) {
-		getRepository().deleteById(id);
+		try {
+			getRepository().deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 
 	public User updateUser(Long id, User user) {
